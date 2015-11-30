044bad6925ba5a40a6e7c32657616039f4da531a
Siebren Bakker <sbakker@salesforce.com>
2015-11-30 15:32:30 -0500

    Pull image from dockerhub instead of building it.
    Image that was created via Dockerfile is now
    available on dockerhub, simplifies process.

--- run.sh
+++ run.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/env bash
-if [ ! -d "$PWD/docker-nginx-rtmp" ]; then
-    if hash git 2>/dev/null; then
-        # Checkout the dockerfile repo
-        git clone https://github.com/AevumDecessus/docker-nginx-rtmp.git
-    else
-        echo "GIT is not installed, please install it before running"
-        exit 1
-    fi
-else
-    echo "docker-nginx-rtmp directory found, not pulling again"
-fi
 if hash docker-compose 2>/dev/null; then
     # Run our server
     docker-compose up -d
