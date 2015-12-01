a7f18c9f7728614a58c6bd883a279cb6243b1f1b
AevumDecessus <decessus.fatum@gmail.com>
2015-11-30 19:01:19 -0500

    Add more options and logging to the run script

--- run.sh
+++ run.sh
@@ -1,7 +1,53 @@
 #!/usr/bin/env bash
+DELETE=0
+LOGGING=0
+STOP=0
+
+usage()
+{
+    cat <<EOF
+Usage :$0 [options]
+-d    Delete the container after stopping (requires -l and -s)
+-h    Prints this help text and exits
+-l    Connects to the container logs after starting
+-s    Stop the container after logging is exite (requires -l)
+EOF
+}
+while getopts dhls opt; do
+    case $opt in
+        h)
+            usage
+            exit 1
+            ;;
+        d)
+            DELETE=1
+            ;;
+        l)
+            LOGGING=1
+            ;;
+        s)
+            STOP=1
+            ;;
+    esac
+done
+if [ -d "$PWD/recordings" ]; then
+    chmod 777 $PWD/recordings
+else
+    mkdir $PWD/recordings
+    chmod 777 $PWD/recordings
+fi
 if hash docker-compose 2>/dev/null; then
     # Run our server
     docker-compose up -d
+    if [ $LOGGING = 1 ]; then
+        docker-compose logs
+        if [ $STOP = 1 ]; then
+            docker-compose stop
+            if [ $DELETE = 1 ]; then
+                docker-compose rm -vf
+            fi
+        fi
+    fi
 else
     echo "docker-compose is not installed, please install it before running"
     exit 1
